f87d2257d7811accc485de2feb5aa33822f6e6b6
yasharzavary <yasharzavary@yasharzavarys-Laptop.local>
2024-06-01 03:14:32 +0330

    sign up function added to the project

--- darslab/apps/signIO/urls.py
+++ darslab/apps/signIO/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 import apps.signIO.views as view
 
 urlpatterns = [
-    path('sign_in', view.signI, name='sign-in')
+    path('sign_in', view.signI, name='sign-in'),
+    path('sign_out', view.signO, name='sign-out'),
 ]
