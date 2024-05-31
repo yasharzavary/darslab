76452455eed0d5eded772cd58ab66b5fe62175a9
yasharzavary <yasharzavary@yasharzavarys-Laptop.local>
2024-06-01 02:52:33 +0330

    sign in part finish, update

--- darslab/darslab/urls.py
+++ darslab/darslab/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.main.urls')),
+    path('sign', include('apps.signIO.urls')),
 ]
