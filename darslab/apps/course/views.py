d6995772c29a7e56fcbcbba824ffd08a1264235c
yasharzavary <yasharzavary@yasharzavarys-Laptop.local>
2024-06-01 11:17:55 +0330

    media url added to the course functions

--- darslab/apps/course/views.py
+++ darslab/apps/course/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from apps.course.models import Course
+from django.conf import settings
 
 
 def courses(req):
     courses = Course.objects.all()
     context = {
+        'media-url': settings.MEDIA_URL,
         'course': courses,
     }
     return render(req, 'course/cors.html', context)
